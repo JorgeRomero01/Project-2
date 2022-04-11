6334cfb2dae801808703131ba5e9547adfe18f90
JorgeRomero01 <jrome140@fiu.edu>
2022-04-11 14:18:52 -0400

    NumberTile Game

---
+++ project#2/src/NumberTile.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList ;
+import java.util.Random;
+
+// A NumberTile is a square tile with a number from 1 to 9 on each side
+public class NumberTile 
+{
+    private ArrayList<Integer> tile ;    // the 4-sided tile
+    private static final Random gen = new Random() ;
+      
+    // Create a NumberTile object with 4 random ints in the range 1 to 9
+    public NumberTile() 
+    {
+       // TO DO: Code the body of this method
+    	tile = new ArrayList();
+    	int topNum = gen.nextInt(9) + 1;
+    	int bottomNum = gen.nextInt(9) + 1;
+    	int rightNum = gen.nextInt(9) + 1;
+    	int leftNum = gen.nextInt(9) + 1;
+    	
+    	tile.add(0, leftNum);
+    	tile.add(1 , topNum);
+    	tile.add(2 , rightNum);
+    	tile.add(3 , bottomNum);
+    	
+    }
+    
+    // Rotate this NumberTile 90 degrees
+    public void rotate() 
+    {
+    	
+       // TO DO: Code the body of this method
+    }
+    
+    // Return the number on the left side of this NumberTile
+    public int getLeft()
+    {
+    	// Do NOT modify this method
+        return tile.get(0) ;
+    }
+    
+    // Return the number on the right side of this NumberTile
+    public int getRight() 
+    {
+        // Do NOT modify this method
+        return tile.get(2) ;
+    }
+    
+    // Return this NumberTile as a multiline string in the form:
+    //     9
+    //  3     7
+    //     6
+    //
+    public String toString() 
+    {
+       // TO DO: Code the body of this method
+       
+       // temporary return statement so program skeleton will compile and run
+       return null ;
+    }    
+} // end of NumberTile class
